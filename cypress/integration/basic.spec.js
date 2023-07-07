01b5d8348da14f2f215cdf9db3b23e531516374e
Mariana Nascimento <mariana.nascimento@sempreit.com.br>
2023-07-07 15:46:40 -0300

    acessar página e interagir com elementos

---
+++ cypress/integration/basic.spec.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+
+describe('Cypress basics', () => {
+    it.only('Should visit a page and assert title', () => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+
+        cy.title().should('be.equal', 'Campo de Treinamento')
+        cy.title().should('contain', 'Campo')
+
+        cy.title()
+            .should('be.equal', 'Campo de Treinamento')
+            .and('contain', 'Campo')
+
+
+
+
+        //TODO imprimir o log no console
+        //TODO escrever o log em um campo de texto
+    })
+
+    it('Should find and interact with an element', () => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+
+        cy.get('#buttonSimple')
+            .click()
+            .should('have.value', 'Obrigado!')
+
+    })
+})
