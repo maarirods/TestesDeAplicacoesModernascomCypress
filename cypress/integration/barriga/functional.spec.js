92acac4f09304c1a6a08e447a56d816e6ee857a2
maarirods <rodspersonal22@gmaail.com>
2023-08-30 14:26:19 -0300

    alteracoes

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -7,46 +7,53 @@ import '../../support/commandsContas.js'
 describe('Should test at a functional level', () => {
     before(() => {
         cy.login('rodspersonal22@gmail.com', '45QDKHe3adggiUx')
-        cy.resetApp()
+
     })
 
-    
+    beforeEach(() => {
+        cy.resetApp()
+        cy.get(loc.MENU.HOME).click()
+
+    })
+
+
     it('Should create an account', () => {
-        cy.acessarMenuConta()
-        cy.inserirConta('Conta Travel')
-        cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso!')
-        
+        cy.acessarMenuConta().click()
+        cy.inserirConta('Conta de Teste')
+        cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso')
+
     })
 
     it('Should update an account', () => {
         //cy.get(':nth-child(4) > :nth-child(2) > :nth-child(1) > .fa-edit')
-        cy.acessarMenuConta()
-        cy.xpath(loc.CONTAS.FN_XP_BTN_ALTERAR('Conta de Teste')).click()
+        cy.acessarMenuConta().click()
+
+        cy.xpath(loc.CONTAS.FN_XP_BTN_ALTERAR('Conta para alterar')).click()
         cy.get(loc.CONTAS.NOME)
-        .clear()
-        .type('Conta alterada')
-        cy.get(loc.CONTAS.BTN_SALVAR).click()
-        cy.get(loc.MESSAGE).should('contain', 'Conta atualizada com sucesso!')
+            .clear()
+            .type('Conta alterada')
+        cy.get(loc.CONTAS.BTN_SALVAR).click({ force: true })
+        cy.get(loc.MESSAGE).should('contain', 'Conta atualizada com sucesso')
 
     })
 
     it('Should not create an account with same name', () => {
-        cy.acessarMenuConta()
+        cy.acessarMenuConta().click()
 
         cy.get(loc.CONTAS.NOME).type('Conta mesmo nome')
         cy.get(loc.CONTAS.BTN_SALVAR).click()
         cy.get(loc.MESSAGE).should('contain', 'code 400')
     })
-    
+
     it('Should create a transaction', () => {
-        cy.get(loc.MENU.MOVIMENTACAO).click({force: true});
+        cy.get(loc.MENU.MOVIMENTACAO).click({ force: true });
 
         cy.get(loc.MOVIMENTACAO.DESCRICAO).type('Desc')
         cy.get(loc.MOVIMENTACAO.VALOR).type('123')
         cy.get(loc.MOVIMENTACAO.INTERESSADO).type('Inter')
         cy.get(loc.MOVIMENTACAO.CONTA).select('Conta para movimentacoes')
-        cy.get(loc.MOVIMENTACAO.STATUS).click();
-        cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click();
+        cy.get(loc.MOVIMENTACAO.STATUS).click()
+        cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
         cy.get(loc.MESSAGE).should('contain', 'sucesso')
 
         cy.get(loc.EXTRATO.LINHAS).should('have.length', 7)
@@ -59,20 +66,22 @@ describe('Should test at a functional level', () => {
 
         cy.get(loc.MENU.EXTRATO).click()
         cy.xpath(loc.EXTRATO.FN_XP_ALTERAR_ELEMENTO('Movimentacao 1, calculo saldo')).click()
-        // cy.wait(1000)
-        cy.get(loc.MOVIMENTACAO.DESCRICAO).should('have.value', 'Movimentacao 1, calculo saldo')
+        cy.wait(1000)
         cy.get(loc.MOVIMENTACAO.STATUS).click()
         cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
         cy.get(loc.MESSAGE).should('contain', 'sucesso')
 
         cy.get(loc.MENU.HOME).click()
-        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta para saldo')).should('contain', '4.034,00')
+        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta para saldo')).should('contain', '4034,00')
+                
     })
+
 
     it('Should remove a transaction', () => {
         cy.get(loc.MENU.EXTRATO).click()
         cy.xpath(loc.EXTRATO.FN_XP_REMOVER_ELEMENTO('Movimentacao para exclusao')).click()
         cy.get(loc.MESSAGE).should('contain', 'sucesso')
     })
-    
+
 })
+
